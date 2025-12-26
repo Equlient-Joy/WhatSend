2640065bb8370155f1710a407112c63a25c42c82
Joy Mondal <joymondal11052006@outlook.com>
2025-12-26 20:43:59 +0530

    chore: add root error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,24 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <html lang="en">
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+        <pre>{error.stack}</pre>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
   return (
     <html lang="en">
